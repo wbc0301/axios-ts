fe1fb33f9272bc61a2210b641d639f8e0df364fd
wbc <417780894@qq.com>
2020-04-11 20:31:31 +0800

    利用XMLHttpRequest发送请求，基础请求demo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { AxiosRequestConfig } from './types';
+import xhr from './xhr';
 
 function axios(config: AxiosRequestConfig) {
-  // TODO
+  xhr(config);
 }
 
 export default axios
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
 export type Method = | 'get' | 'GET' | 'delete' | 'DELETE' | 'head' | 'HEAD' | 'options' | 'OPTIONS' | 'post' | 'POST' | 'put' | 'PUT' | 'patch' | 'PATCH'
 
 export interface AxiosRequestConfig {
-  url?: string
+  url: string
   method?: Method
   data?: any
   params?: any
